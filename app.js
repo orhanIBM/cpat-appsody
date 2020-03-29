4297236f7688d4e7bd20b15cd2a371f2c028329f
Orhan-Yarar <orhan.yarar@ibm.com>
2020-03-29 16:27:45 -0400

    Tekton pipeline run take 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! vIBM Cloud");
+  res.send("Hello from Appsody! vTekton-take #2");
 });
  
 module.exports.app = app;
