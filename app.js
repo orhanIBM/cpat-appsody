ca391a9f5984b6bd4f7185092544b95310b81780
Orhan-Yarar <orhan.yarar@ibm.com>
2020-03-29 15:53:16 -0400

    code update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! v2");
+  res.send("Hello from Appsody! vIBM Cloud");
 });
  
 module.exports.app = app;
