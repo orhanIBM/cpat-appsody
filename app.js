8a305a181c504908429fddf1de4949c6a861cfc0
Orhan-Yarar <orhan.yarar@ibm.com>
2020-03-29 22:06:24 -0400

    Appsody + Tekton take 5, dave brubeck

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! vTekton-take #4");
+  res.send("Hello from Appsody! vTekton-take #5 dave brubeck this time!");
 });
  
 module.exports.app = app;
